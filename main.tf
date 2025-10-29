877bd0aeb00d9ff449da02bbed3302808c6aec1f
Abhay Kumar Saini <abhaykumarsaini9982@gmail.com>
2025-10-29 13:30:45 +0530

    fix: Remove insecure S3 bucket

--- main.tf
+++ main.tf
@@ -19,15 +19,3 @@ provider "aws" {
 resource "aws_s3_bucket" "insecure_bucket" {
   bucket = "my-insecure-tfsec-test-bucket-12345" # Make sure this is still unique
 }
-
-# This is the NEW insecure part. We are explicitly
-# turning OFF all the "block public access" settings.
-resource "aws_s3_bucket_public_access_block" "insecure_block" {
-  bucket = aws_s3_bucket.insecure_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-# --- END OF INSECURE RESOURCE ---
