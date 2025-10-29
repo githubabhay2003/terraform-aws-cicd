b369611bf4d6beab934c4c4fba31c80828680038
Abhay Kumar Saini <abhaykumarsaini9982@gmail.com>
2025-10-29 14:15:34 +0530

    feat: Add infracost cost estimation

--- main.tf
+++ main.tf
@@ -12,3 +12,26 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+# --- ADD THIS RESOURCE FOR COST ESTIMATION ---
+# (We will remove it later)
+
+# 1. Get the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# 2. Create a t3.micro EC2 instance
+resource "aws_instance" "test_vm" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t3.micro" # This has a small cost
+
+  tags = {
+    Name = "cost-estimation-test"
+  }
+}
+# --- END OF COST RESOURCE ---
