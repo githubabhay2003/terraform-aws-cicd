667c8017c4566409f7df1da3caf799c7924cc8f5
Abhay Kumar Saini <abhaykumarsaini9982@gmail.com>
2025-10-29 15:22:19 +0530

    feat: Add random pet for final test

--- main.tf
+++ main.tf
@@ -5,6 +5,10 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+      random = {
+        source  = "hashicorp/random"
+        version = "~> 3.0"
+      }
     }
   }
 }
@@ -12,3 +16,8 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+# --- ADD THIS RESOURCE FOR FINAL TEST ---
+resource "random_pet" "test" {
+  length = 2
+}
+# --- END OF TEST RESOURCE ---
