082714f091815a4e5687aa2c3ec3d5404262ad4a
Abhay Kumar Saini <abhaykumarsaini9982@gmail.com>
2025-10-29 13:11:10 +0530

    feat: Add tfsec security scanning

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # Lock to a major version
+      version = "~> 5.0"
     }
   }
 }
@@ -12,3 +12,22 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# --- ADD THIS INSECURE RESOURCE ---
+# This S3 bucket is deliberately insecure for testing tfsec
+
+resource "aws_s3_bucket" "insecure_bucket" {
+  bucket = "my-insecure-tfsec-test-bucket-12345" # Make sure this is still unique
+}
+
+# This is the NEW insecure part. We are explicitly
+# turning OFF all the "block public access" settings.
+resource "aws_s3_bucket_public_access_block" "insecure_block" {
+  bucket = aws_s3_bucket.insecure_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+# --- END OF INSECURE RESOURCE ---
