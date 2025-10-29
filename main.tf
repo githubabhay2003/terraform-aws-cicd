d5d5b0edb0acf31144d01df9c8fe8ee8dd9138d3
Abhay Kumar Saini <abhaykumarsaini9982@gmail.com>
2025-10-29 13:32:48 +0530

    fix: Remove insecure S3 bucket

--- main.tf
+++ main.tf
@@ -12,10 +12,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-# --- ADD THIS INSECURE RESOURCE ---
-# This S3 bucket is deliberately insecure for testing tfsec
-
-resource "aws_s3_bucket" "insecure_bucket" {
-  bucket = "my-insecure-tfsec-test-bucket-12345" # Make sure this is still unique
-}
